2834bcccf868a90db5854ed8d9e5b7aa47f2ffe1
Alexander Mironov <alexandrfox@gmail.com>
2011-04-11 20:34:22 +0700

    Added Data Structures (descriptors struct's). Project cleaned.

---
+++ Graphs/DataStructures.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Graphs
+{
+    /// <summary>
+    /// Дескриптор вершины
+    /// </summary>
+    struct VertexDescriptor
+    {
+        /*
+        public enum Colors { Black, White, None }
+        public Colors color;
+        */
+        public string name;
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="n">Имя вершины</param>
+        public VertexDescriptor(string n)
+        {
+            name = n;
+        }
+    }
+    
+    /// <summary>
+    /// Дескриптор ребра
+    /// </summary>
+    struct EdgeDescriptor
+    {
+        public double weight;
+
+        /// <summary>
+        /// Конструктор структуры
+        /// </summary>
+        /// <param name="w">Вес ребра</param>
+        public EdgeDescriptor(double w)
+        {
+            weight = w;
+        }
+    }
+}
